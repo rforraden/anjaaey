19c17eb6c53818387d01be7fab3df1f0866b4667
rforraden <158191408+rforraden@users.noreply.github.com>
2024-06-02 22:21:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "aaaa okayy! goodluck! mau kapann?";
+  gif.src =
+    "6371c9b1-3c79-433e-b875-72e6e3553bd1.jpg";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
